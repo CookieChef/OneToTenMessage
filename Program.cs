d3ccc70fef31db3bffbe2be12eb1e5009c726b60
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-15 22:57:42 -0400

    completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace OneToTenMessage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true) {
+                Console.WriteLine("Enter a number between 1 and 10");
+                var userNum = Int32.Parse(Console.ReadLine());
+                while(userNum > 0) {
+                    Console.WriteLine("Good Job!");
+                    userNum --;
+                }
+            }
         }
     }
 }
